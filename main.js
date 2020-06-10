6f59487d4ed62739da8cf1a6e4abe01c3347bda1
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-06-10 19:12:58 +0530

    called bfs function

---
+++ main.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const {bfs} = require('./src/graph');
+
+const main = () => {
+  const data = fs.readFileSync('./data.txt', 'utf8').split('\n');
+  const content = data.map(e => e .split('|') .map(e => e.trim()) .splice(1, 2) );
+  const result = bfs(content,'mm','jj');
+  console.log(result);
+};
+
+main();
