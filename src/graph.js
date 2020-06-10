bcb98013be2706f7c8a4473e3fbd8e4486991d6a
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-06-10 19:12:25 +0530

    added bfs and createList function

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,27 @@
-//Example 
-// Pairs => [[from,to],[to,from]]
-// Source => from
-// To => to 
-// Should return true.
-
-const bfs = function(pairs,source,target){
-  
+const createList = content => {
+  const list = {};
+  content.forEach(pair => {
+    if (!list[pair[0]]) list[pair[0]] = [pair[1]];
+    else list[pair[0]].push(pair[1]);
+  });
+  return list;
 };
 
-module.exports = {bfs};
+const bfs = (pairs, source, target) => {
+  const adjacentList = createList(pairs);
+  const visitedList = [];
+  const queue = [];
+  queue.push(source);
+  while (queue.length != 0) {
+    const currentPath = queue.shift();
+    if (currentPath === target) return true;
+    visitedList.push(currentPath);
+    adjacentList[currentPath].forEach(path => {
+      if (!visitedList.includes(path) && !queue.includes(path))
+        queue.push(path);
+    });
+  }
+  return false;
+};
+
+module.exports = { bfs };
