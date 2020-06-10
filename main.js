f46052b7abf7674a61ddfa12b8188107b1297a1c
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-06-10 19:28:32 +0530

    added updated test condition

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {bfs} = require('./src/graph');
 const main = () => {
   const data = fs.readFileSync('./data.txt', 'utf8').split('\n');
   const content = data.map(e => e .split('|') .map(e => e.trim()) .splice(1, 2) );
-  const result = bfs(content,'mm','jj');
+  const result = bfs(content,'bb','jj');
   console.log(result);
 };
 
