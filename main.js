13446c1d05347e86607f7a7382e5ae899c3f1af7
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-06-11 08:38:55 +0530

    modified main

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const {bfs} = require('./src/graph');
+const { bfs } = require('./src/graph');
 
 const main = () => {
   const data = fs.readFileSync('./data.txt', 'utf8').split('\n');
   const pairs = data.map(e => e .split('|') .map(e => e.trim()) .splice(1, 2) );
-  const result = bfs(pairs,'ee','mm');
+  const result = bfs(pairs, 'ee', 'mm');
   console.log(result);
 };
 
